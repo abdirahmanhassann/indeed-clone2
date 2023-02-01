d99ef0d1e484ce2635bbea349e5f5922f6204258
unknown <abdi12340.am@gmail.com>
2023-02-01 19:51:01 +0300

    stored job posting state Upload to  post to database as array

--- src/Components/SigninComponents/Employer/EmployerPostjob.js
+++ src/Components/SigninComponents/Employer/EmployerPostjob.js
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Paragraph from '../../../ElementComponents/paragraph'
 import EmployerNav from './EmployerNav'
 import picc from '../../../img/Employer-Frequently-Asked-Questions.png'
 import Header from '../../../ElementComponents/Header'
 import Largeheader from '../../../ElementComponents/Largeheader'
-import { TextField } from '@mui/material'
+import { Checkbox, FormControl, FormControlLabel, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, TextareaAutosize, TextField } from '@mui/material'
 import Subaparagraph from '../../../ElementComponents/subaparagraph'
 import BlueButton from '../../../ElementComponents/bluebutton'
 import InverseButton from '../../../ElementComponents/InverseButton'
+import pic2 from '../../../img/indeed-Hub-illustrations-How-to-Consistently-Attract-and-Filter-Quality-Applicants.png'
+import { useSelector } from 'react-redux'
+import { arrayUnion, collection, doc, getDocs, setDoc } from '@firebase/firestore'
+import { db } from '../../../Firebase/Firebase'
+// import { Textarea } from '@mui/joy'
 function Postjob() {
+  const [formm,setform]=useState({title:'',location:'',rate:'',min:null,max:null,description:'',time:''});
+const selector=useSelector((state)=>state.reducer.employeremailstatus.employeremail.email)
+
+  function changed(e){
+    console.log(formm)
+    setform(i=>{
+      return{
+        ...i,
+        [e.target.name]:e.target.value,
+            }
+    })
+  }
+
+  async function clicked(){
+
+    if(formm.title.length >4 ||formm.location.length >=2 ||formm.rate ||formm.min >0 ||formm.max > 0 ||formm.description.length >4 || formm.time)
+    {
+      const  usersCollectionRef= await collection (db,'employer')
+      const po=  await getDocs(usersCollectionRef)
+      const  userss= await po.docs.map((i)=>{return{...i.data(),id:i.id}})
+    await  console.log(userss);
+    const check= await userss.find(i=>i.email==selector)
+      if (check) {
+     const p = await setDoc(doc(db,'employer',check.id),{jobpostings:arrayUnion(formm) },{merge:true})
+
+      }
+      else { 
+        console.log('doesnt exist')
+    }
+  }}
   return (
     <div>
         <>
@@ -20,20 +55,90 @@ function Postjob() {
             </div>
             <div className='postjobsubdiv2'>
 <Header text={'Job title'}/>
-<TextField id="outlined-basic" label="Job title" variant="outlined" sx={{width:'90%'}}/>
+<TextField id="outlined-basic" label="Job title" variant="outlined" sx={{width:'90%'}}
+name='title' value={formm.title}
+onChange={changed}
+/>
             </div>
             <div className='postjobsubdiv2'>
 <Header text={'Where will the employee report to work?'}/>
-<TextField id="outlined-basic" label="Location" variant="outlined" sx={{width:'90%'}}/>
+<TextField id="outlined-basic" label="Location" variant="outlined" sx={{width:'90%'}}
+name='location' value={formm.location} 
+onChange={changed}
+/>
             </div>
             <div className='postjobsubdiv2'>
+
 <Header text={'What is the salary you are willing to provide?'}/>
-<Subaparagraph text={'GBP is the default currency'}/>
-<TextField id="outlined-basic" label="salary" type='number' variant="outlined" sx={{width:'90%'}}/>
+<Subaparagraph text={'Consider GBP the currency'}/>
+<div className='postjobsubdiv'>
+<FormControl fullWidth>
+  <InputLabel id="demo-simple-select-label">Rate</InputLabel>
+  <Select
+    labelId="demo-simple-select-label"
+    id="demo-simple-select"
+    name='rate' value={formm.rate}
+    label="Rate"
+    onChange={changed}
+  >
+    <MenuItem value={'hourly'} >Hourly</MenuItem>
+    <MenuItem value={'monthly'} >Monthly</MenuItem>
+    <MenuItem value={'annual'}>Annually</MenuItem>
+  </Select>
+</FormControl>
+<TextField id="outlined-basic" label="Minimum" type='number' variant="outlined" sx={{width:'100%'}} 
+name='min' value={formm.min}
+onChange={changed}
+/>
+<Paragraph text={'to'}/>
+<TextField id="outlined-basic" label="Maximum" type='number' variant="outlined" sx={{width:'100%'}}
+name='max' value={formm.max}
+onChange={changed}
+/>
+</div>
             </div>
             <div className='postjobsubdiv'>
+<Largeheader text={'Describe the job'} style={{fontSize:'25px'}}/>
+<img src={pic2} className='postjobpic'/>
+            </div>
+            <div className='postjobsubdiv2'>
+<Header text={'Job description'}/>
+<TextareaAutosize
+
+  color="neutral"
+  disabled={false}
+  minRows={4}
+  placeholder="Please provide a brief description on the nature of the position"
+  variant="outlined"
+  style={{minHeight:'50px',maxWidth:'100%',minWidth:'100%',border:'1px solid rgb(22, 64, 129)'
+  ,outline:'none',fontSize:'18px',fontFamily:'roboto',borderRadius:'10px'}}
+  name='description' value={formm.description}
+  onChange={changed}
+/>
+            </div>
+
+          
+ <div className='postjobsubdiv2'>
+<Header text={'Shift times'}/>
+  <FormControl>
+  <RadioGroup
+    aria-labelledby="demo-radio-buttons-group-label"  
+    name="time"
+    value={formm.time}
+onChange={changed}
+    sx={{display:'flex',flexDirection:'row'}}
+
+  >
+    <FormControlLabel value='fulltime' control={<Radio />} label="Full-time" />
+    <FormControlLabel value='Part-time' control={<Radio />} label="Part-time" />
+    <FormControlLabel value='Temporary' control={<Radio />} label="Temporary" />
+  </RadioGroup>
+</FormControl>
+            </div>
+
+            <div className='postjobsubdiv'>
 <InverseButton text={'Back'}/>
-<BlueButton text = {'Continue'}/>
+<BlueButton text = {'Continue'} click={clicked}/>
             </div>
             
         </div>
