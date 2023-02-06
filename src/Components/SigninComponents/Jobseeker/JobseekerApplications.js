e04f7ed81ba8d6ac4c067cfe03113e6ecaf41c6e
unknown <abdi12340.am@gmail.com>
2023-02-07 02:01:53 +0300

    Added remove job button in jobseeker job Applicantions component.

--- src/Components/SigninComponents/Jobseeker/JobseekerApplications.js
+++ src/Components/SigninComponents/Jobseeker/JobseekerApplications.js
@@ -1,4 +1,4 @@
-import { collection, getDocs } from '@firebase/firestore'
+import { arrayRemove, collection, doc, getDocs, updateDoc } from '@firebase/firestore'
 import moment from 'moment'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
@@ -10,13 +10,16 @@ import Paragraph from '../../../ElementComponents/paragraph'
 import Paragraphblue from '../../../ElementComponents/paragraphblue'
 import Subaparagraph from '../../../ElementComponents/subaparagraph'
 import { db } from '../../../Firebase/Firebase'
-import Nav from '../../GeneralComponents/Nav'
+import Nav from '../../GeneralComponents/Nav';
+import ScaleLoader from "react-spinners/ClipLoader";
+
 import './Jobseeker.css'
 function JobseekerApplications() {
     const jobseekerlogin=useSelector(state=>state.reducer.jobseekerloginstatus.jobseekerlogin);
     const jobseekeremaill=useSelector((state)=>state.reducer.jobseekeremailstatus.jobseekeremail);
     const [isloading,setisloading]=useState(false);
     const [info,setinfo]=useState()
+    const[ischange,setischange]=useState(false);
     useEffect(()=>{
         async function details(){
           setisloading(true)
@@ -28,10 +31,13 @@ function JobseekerApplications() {
           if (check) {
 console.log(check)
 setinfo(check)
+setisloading(false)
         }
     }
  details()   
-},[])
+},[ischange])
+
+
   return (
 <>
 <Nav/>
@@ -39,6 +45,16 @@ setinfo(check)
 jobseekerlogin==false ?
 <Largeheader text={'please login'}/>
 :
+isloading ? 
+<div className="loader" style={{justifySelf:'center'}}><ScaleLoader
+ size={150}
+ margin={'auto'}
+ color={'#2557a7'}
+ borderwidth= {'7px'}
+ 
+/></div>
+    :
+
 <div className='largedivapp' style={{background:'white',width:'84%'}}>
 {/* <<h1 className='headerjobapplication'>Find jobs</h1> */}
 <div className='columndiv'>
@@ -52,7 +68,7 @@ info.jobpostings.map((i)=>{
 <>
 <div className='postjobsubdiv' style={{borderBottom:'1px solid lightgray'}}>
 <div className='columndiv'>
-<Header text={i.title}/>
+<Header text={i.title} style={{margin:'-3px'}}/>
 <Paragraphblue text={i.name} style={{marginBlock:'14px'}}/>
 <Subaparagraph text={i.location}/>
 <Subaparagraph text={'Created '+timeago}/>
@@ -70,7 +86,10 @@ i.accepted==false &&
    <p className='applied'>Applied</p>
   }
   <div style={{minWidth:'255px'}}>
-<BlueButton text={'Remove job'}/>
+<BlueButton text={'Remove job'} click={()=>{
+              updateDoc(doc(db,'jobseeker',info.id),({jobpostings:arrayRemove(i)}))
+              setischange(i=>!i)
+}}/>
   </div>
 </div>
 </>
