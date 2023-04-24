ee7f92ccfb4b898118617df67aa69972ba2f0797
unknown <abdi12340.am@gmail.com>
2023-04-24 15:42:20 +0300

    Added update cv function

--- src/Components/SigninComponents/Jobseeker/JobseekerUpdate.js
+++ src/Components/SigninComponents/Jobseeker/JobseekerUpdate.js
@@ -12,13 +12,15 @@ import picc from '../../../img/Employer-Frequently-Asked-Questions.png'
 import pic2 from '../../../img/indeed-Hub-illustrations-How-to-Consistently-Attract-and-Filter-Quality-Applicants.png'
 import { useNavigate } from 'react-router-dom';
 import { arrayRemove, arrayUnion, collection, doc, getDocs, setDoc, updateDoc } from '@firebase/firestore';
-import { db } from '../../../Firebase/Firebase';
+import { db, storagee } from '../../../Firebase/Firebase';
+import { getDownloadURL, ref, uploadBytes } from '@firebase/storage';
 
 function JobseekerUpdate() {
     const email=useSelector(state=>state.reducer.jobseekeremailstatus.jobseekeremail);
     const [form,setform]=useState()
     const navigate=useNavigate();
 const [edited,setedited]=useState(false)
+const [cvupload,setcvupload]=useState(null)
 function changed(e){
     setform(i=>{
         return {
@@ -30,18 +32,27 @@ function changed(e){
 }
 async function clicked(){
     const newobj={
+
         email:form.email,
         city:form.city? form.city : form.city,
         country:form.country? form.country : form.country,
         startdate:form.startdate? form.startdate:form.startdate,
         enddate:form.enddate? form.enddate: form.enddate,
-        role:form.role? form.role: form.role,
+        role:form.role? form.role: form.role 
     }
 
     await setDoc(doc(db,'jobseeker',form.id),{
         Firstname:form.Firstname,Surname:form.Surname,country:form.country,city:form.city,email:form.email
     ,RecentExperience:form.RecentExperience,startdate:form.startdate,enddate:form.enddate,role:form.role
     },{merge:true})
+
+    if (cvupload){
+        const imagge= ( ref(storagee,`/jobseeker/${email}`))
+        console.log(imagge)
+        console.log(storagee)
+        await uploadBytes(imagge, cvupload)
+    }
+
 setedited(true)
 }
 useEffect(()=>{
@@ -63,7 +74,7 @@ useEffect(()=>{
         <>
         <Nav/>
       {  form &&
-        <div className='largedivpostjob'>
+        <div className='largedivpostjob' style={{marginTop:'-58px'}}>
             <div className='postjobsubdivAlt'>
             
 <Largeheader text={`${form.Firstname}  ${form.Surname}`} style={{fontSize:'25px'}}/>
@@ -98,11 +109,23 @@ onChange={changed}
 name='role' defaultValue={form.role}
 onChange={changed}
 />
+<Header text={'View or change CV'} />
+<div className='postjobsubdivAlt'>
+<InverseButton text={'View CV'} click={()=> { 
+        getDownloadURL(ref(storagee, `jobseeker/${email}`)).then((url)=>
+        {
+           window.open(url)
+        })
 
+}}/>
+<input style={{height:'inherit'}} className='BlueButton' type="file"  accept=".pdf" onChange={(e)=>{
+setcvupload(e.target.files[0])
+  }}/>
+</div>
             </div>
             <div className='postjobsubdivAlt'>
 <InverseButton text={'Back'} click={()=>navigate(`../`)}/>
-<BlueButton text = {'Edit'}  click={()=>clicked()}/>
+<BlueButton text = {'Save changes'}  click={()=>clicked()}/>
             </div>
             {
                 edited &&
